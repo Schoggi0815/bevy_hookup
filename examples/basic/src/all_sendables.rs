68396a47e90cc2b0a3653b9ac5ab86641fba5e08
Schoggi <mail@mbossi.ch>
2025-09-07 15:59:06 +0200

    remove reflect for Sendables

--- examples/basic/src/all_sendables.rs
+++ examples/basic/src/all_sendables.rs
@@ -1,9 +1,8 @@
-use bevy::reflect::Reflect;
 use serde::{Deserialize, Serialize};
 
 use crate::{sync_name::SyncName, test_component::TestComponent, test_component_2::TestComponent2};
 
-#[derive(Reflect, Clone, Serialize, Deserialize)]
+#[derive(Clone, Serialize, Deserialize)]
 pub enum Sendables {
     TestComponent(TestComponent),
     TestComponent2(TestComponent2),
